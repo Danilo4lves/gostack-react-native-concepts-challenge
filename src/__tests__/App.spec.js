53c07222cd6f364fa1a5bd898f5dacb2ddcc6222
Danilo Alves <danilovieiraalves@hotmail.com>
2020-08-16 00:41:52 -0300

    fix(__tests__): Fixes importation of api

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,12 +1,16 @@
+// Dependencies
 import "react-native";
 import React from "react";
-
 import AxiosMock from "axios-mock-adapter";
-import api from "../services/api";
-const apiMock = new AxiosMock(api);
-
-import App from "../App";
 import { render, fireEvent, act } from "@testing-library/react-native";
+
+// Api
+import { api } from "../config";
+
+// App
+import App from "../App";
+
+const apiMock = new AxiosMock(api);
 
 const wait = (amount = 0) => {
   return new Promise((resolve) => setTimeout(resolve, amount));
@@ -18,7 +22,7 @@ const actWait = async (amount = 0) => {
   });
 };
 
-const repositoryId = 'd6e43105-a559-45b7-8fd7-53416b415741';
+const repositoryId = "d6e43105-a559-45b7-8fd7-53416b415741";
 
 apiMock.onGet("repositories").reply(200, [
   {
@@ -49,15 +53,13 @@ describe("Likes", () => {
   it("should add a like to the like counter of the repository", async () => {
     const { getByTestId } = render(<App />);
 
-    apiMock
-      .onPost(`repositories/${repositoryId}/like`)
-      .reply(200, {
-        id: repositoryId,
-        title: "Desafio React Native",
-        url: "https://github.com/josepholiveira",
-        techs: ["React Native", "Node.js"],
-        likes: 1,
-      });
+    apiMock.onPost(`repositories/${repositoryId}/like`).reply(200, {
+      id: repositoryId,
+      title: "Desafio React Native",
+      url: "https://github.com/josepholiveira",
+      techs: ["React Native", "Node.js"],
+      likes: 1,
+    });
 
     await actWait();
 
@@ -65,22 +67,24 @@ describe("Likes", () => {
 
     await actWait();
 
-    expect(getByTestId(`repository-likes-${repositoryId}`)).toHaveTextContent("1 curtida");
+    expect(getByTestId(`repository-likes-${repositoryId}`)).toHaveTextContent(
+      "1 curtida"
+    );
 
-    apiMock
-      .onPost(`repositories/${repositoryId}/like`)
-      .reply(200, {
-        id: repositoryId,
-        title: "Desafio React Native",
-        url: "https://github.com/josepholiveira",
-        techs: ["React Native", "Node.js"],
-        likes: 2,
-      });
+    apiMock.onPost(`repositories/${repositoryId}/like`).reply(200, {
+      id: repositoryId,
+      title: "Desafio React Native",
+      url: "https://github.com/josepholiveira",
+      techs: ["React Native", "Node.js"],
+      likes: 2,
+    });
 
     fireEvent.press(getByTestId(`like-button-${repositoryId}`));
 
     await actWait();
 
-    expect(getByTestId(`repository-likes-${repositoryId}`)).toHaveTextContent("2 curtidas");
+    expect(getByTestId(`repository-likes-${repositoryId}`)).toHaveTextContent(
+      "2 curtidas"
+    );
   });
 });
